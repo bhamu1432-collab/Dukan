a6309e4c5f253e91051348682f0784d0922feda8
bhamu1432-collab <bhamu1432@gmail.com>
2026-03-19 22:06:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 function toggleMenu() {
   let menu = document.getElementById("menu");
+  let overlay = document.getElementById("overlay");
+
   if (menu.style.left === "0px") {
-    menu.style.left = "-250px";
+    menu.style.left = "-260px";
+    overlay.style.display = "none";
   } else {
     menu.style.left = "0px";
+    overlay.style.display = "block";
   }
+}
+
+function openProduct(name, price) {
+  document.getElementById("popup").style.display = "flex";
+  document.getElementById("pname").innerText = name;
+  document.getElementById("pprice").innerText = price;
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function orderNow() {
+  alert("Order placed for " + document.getElementById("pname").innerText);
 }
