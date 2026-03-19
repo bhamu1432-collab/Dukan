200ff19935a175e2648639c685a5c82968d8cf83
bhamu1432-collab <bhamu1432@gmail.com>
2026-03-19 22:47:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-function toggleMenu() {
-  let menu = document.getElementById("menu");
-  let overlay = document.getElementById("overlay");
-
-  if (menu.style.left === "0px") {
-    menu.style.left = "-260px";
-    overlay.style.display = "none";
-  } else {
-    menu.style.left = "0px";
-    overlay.style.display = "block";
-  }
+function openNav() {
+    document.getElementById("mySidebar").style.width = "280px";
+    document.getElementById("overlay").style.display = "block";
 }
 
-function openProduct(name, price) {
-  document.getElementById("popup").style.display = "flex";
-  document.getElementById("pname").innerText = name;
-  document.getElementById("pprice").innerText = price;
-}
-
-function closePopup() {
-  document.getElementById("popup").style.display = "none";
-}
-
-function orderNow() {
-  alert("Order placed for " + document.getElementById("pname").innerText);
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
 }
